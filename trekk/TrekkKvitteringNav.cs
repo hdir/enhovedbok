7af29c90b2eeb4bea7e9dd13c51767ba7815e74a
Christian Gunvaldsen <gundi88@gmail.com>
2019-05-21 16:22:50 +0200

    Create TrekkKvitteringNav.cs

---
+++ trekk/TrekkKvitteringNav.cs
@@ -0,0 +1,12 @@
+public class TrekkKvitteringNav
+{
+    public int SystemId { get; set; }
+
+    public string ForsystemRef { get; set; }
+
+    public decimal OpprinneligBelop { get; set; }
+
+    public decimal TrukkerBelop { get; set; }
+
+    public DateTime SapTrekkDato { get; set; }
+}
