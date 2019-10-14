e2c97c9f37c59e8001f787b0f62d208a7523e119
Christian Gunvaldsen <gundi88@gmail.com>
2019-10-14 10:44:32 +0200

    Update TrekkKvitteringNav.cs

--- trekk/TrekkKvitteringNav.cs
+++ trekk/TrekkKvitteringNav.cs
@@ -24,3 +24,15 @@ public class TrekkMeldingDto
     [JsonProperty(PropertyName = "trekk", NullValueHandling = NullValueHandling.Ignore)]
     public TrekkTypeOppsummeringDto[] Trekk { get; set; }
 }
+
+public class TrekkTypeOppsummeringDto
+{
+    [JsonProperty(PropertyName = "trekkType", NullValueHandling = NullValueHandling.Ignore)]
+    public string TrekkType { get; set; }
+
+    [JsonProperty(PropertyName = "trekkBelop", NullValueHandling = NullValueHandling.Ignore)]
+    public decimal? TrekkBelop { get; set; }
+
+    [JsonProperty(PropertyName = "saldoTrekkType", NullValueHandling = NullValueHandling.Ignore)]
+    public decimal? SaldoTrekkType { get; set; }
+}
