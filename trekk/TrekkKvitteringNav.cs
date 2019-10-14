9566d153b4314a341610fab8e9bfdd1a3ab6e983
Christian Gunvaldsen <gundi88@gmail.com>
2019-10-14 10:44:14 +0200

    Update TrekkKvitteringNav.cs

--- trekk/TrekkKvitteringNav.cs
+++ trekk/TrekkKvitteringNav.cs
@@ -1,15 +1,26 @@
-using System;
-
-// det sendes en trekk-kvittering per kontakt. Dvs alle forsystemreferanser er knyttet til utbetaling til samme person.
-public class TrekkKvitteringNav
+public class TrekkMeldingDto
 {
+    [JsonProperty(PropertyName = "systemId")]
     public int SystemId { get; set; }
 
+    [JsonProperty(PropertyName = "offisiellId")]
+    public string OffisiellId { get; set; }
+
+    [JsonProperty(PropertyName = "forsystemReferanser")]
     public string[] ForsystemReferanser { get; set; }
 
+    [JsonProperty(PropertyName = "forsystemReferanserFraPeriode")]
+    public string[] AlleForsystemReferanserFraPeriode { get; set; }
+
+    [JsonProperty(PropertyName = "totaltOpprinneligBelop")]
     public decimal TotaltOpprinneligBelop { get; set; }
 
+    [JsonProperty(PropertyName = "totaltTrukketBelop")]
     public decimal TotaltTrukketBelop { get; set; }
 
+    [JsonProperty(PropertyName = "sapTrekkDato")]
     public DateTime SapTrekkDato { get; set; }
+
+    [JsonProperty(PropertyName = "trekk", NullValueHandling = NullValueHandling.Ignore)]
+    public TrekkTypeOppsummeringDto[] Trekk { get; set; }
 }
