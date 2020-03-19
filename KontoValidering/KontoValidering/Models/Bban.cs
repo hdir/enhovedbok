5935c8f816aabaa6ec276074457389e3359d151a
GUNVALDSEN Christian <christian.gunvaldsen@soprasteria.com>
2020-03-19 15:55:39 +0100

    la inn kontovalidering for bruk av eksterne

---
+++ KontoValidering/KontoValidering/Models/Bban.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KontoValidering.Models
+{
+    public class Bban
+    {
+        public string Kontonummer { get; set; }
+
+        public string Swift { get; set; }
+
+        public string Banknavn { get; set; }
+
+        public string[] Bankadresse { get; set; }
+
+        public string Bankkode { get; set; }
+
+        public string Landkode { get; set; }
+    }
+}
