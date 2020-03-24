853bebbcc8a0d719c2ead92fd3d88b21a30abcb3
GUNVALDSEN Christian <christian.gunvaldsen@soprasteria.com>
2020-03-24 15:43:48 +0100

    oppdaterte validering av bban til å sjekke mot gyldige landkoder

--- KontoValidering/KontoValidering/BbanValidator.cs
+++ KontoValidering/KontoValidering/BbanValidator.cs
@@ -1,16 +1,22 @@
 ﻿using KontoValidering.Models;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace KontoValidering
 {
     public static class BbanValidator
     {
-        public static bool GyldigBban(Bban bban)
+        /// <summary>
+        /// Validerer at obligatoriske felter er satt
+        /// </summary>
+        /// <param name="bban">BBAN konto</param>
+        /// <param name="gyldigeLandkoder">Hentes fra ssb http://data.ssb.no/api/klass/v1/classifications/100/codes.json?from={dato}</param>
+        /// <returns></returns>
+        public static bool GyldigBban(Bban bban, HashSet<string> gyldigeLandkoder)
         {
             return !string.IsNullOrWhiteSpace(bban.Kontonummer)
                 && (string.IsNullOrWhiteSpace(bban.Swift) || SwiftErGyldig(bban.Swift))
-                && (!string.IsNullOrWhiteSpace(bban.Landkode) && bban.Landkode.Length == 2)
-                && !string.IsNullOrWhiteSpace(bban.Bankkode)
+                && (!string.IsNullOrWhiteSpace(bban.Landkode) && gyldigeLandkoder.Contains(bban.Landkode))
                 && !string.IsNullOrWhiteSpace(bban.Banknavn)
                 && bban.Bankadresse.Any(x => !string.IsNullOrWhiteSpace(x));
         }
